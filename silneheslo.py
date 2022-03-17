8393cc0730781a9369cf5aed814972f1cc635ba1
eliskahob <101802139+eliskahob@users.noreply.github.com>
2022-03-17 11:33:19 +0100

    program heslo

---
+++ silneheslo.py
@@ -0,0 +1,18 @@
+def silneheslo():
+    heslo = input("napiste heslo,ktere ma alespon 6 znaku a ktere obsahuje mala a velka pismena a cisla, nebo napiste 'vygenerovat nove heslo' pro vygenerovani noveho hesla: ")
+    '''if heslo == "vygenerovat heslo" '''
+    pocet(heslo)
+
+
+def pocet(heslo):
+    if len(heslo) < 6:
+        print ("heslo musi obsahovat alespon 6 znaku, napiste nove heslo: ")
+        silneheslo()
+
+def cisla(heslo):
+    if 
+    
+
+
+
+    
